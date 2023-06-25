1bef1db9af56892bc68ff60772a573cead6d4d12
Shawn Armstrong <smarmstr@ucsc.edu>
2023-06-24 19:28:36 -0700

    Add base field to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
-// Plugins
-import vue from '@vitejs/plugin-vue'
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-
-// Utilities
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Demo4/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
